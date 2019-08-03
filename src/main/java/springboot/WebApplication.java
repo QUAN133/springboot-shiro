1c593bf45dd212255ade358460707a60684c4964
zhengliquann <269045463@qq.com>
2019-08-03 11:17:51 +0800

    first commit

---
+++ src/main/java/springboot/WebApplication.java
@@ -0,0 +1,14 @@
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Quan on 2018/11/16.
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args){
+        SpringApplication.run(WebApplication.class,args);
+    }
+}
